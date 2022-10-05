40ef66a0d6a70a48ebfa5d2b0ddfab823ef38e8a
Lochan Melam <lochan99melam@gmail.com>
2022-10-05 22:35:18 +0530

    Update B - Basic Data Types.cpp

--- B - Basic Data Types.cpp
+++ B - Basic Data Types.cpp
@@ -6,7 +6,7 @@ int main(){
     char c;
     float d;
     double e;
-    
+    cout<<"enter values ";
     cin >> a >> b >> c >> d >> e;
     cout << a << "\n" << b <<"\n"<< c <<"\n"<< d <<"\n"<< e ;
     return 0;
